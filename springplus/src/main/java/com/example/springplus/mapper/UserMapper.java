cb8f6c1aa7bdf3cf34a04720ab65bee110ac06fd
king <349014584@qq.com>
2020-08-16 11:00:19 +0800

    删除了文件夹

--- springplus/src/main/java/com/example/springplus/mapper/UserMapper.java
+++
@@ -1,8 +0,0 @@
-package com.example.springplus.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.springplus.bean.User;
-
-public interface UserMapper extends BaseMapper<User> {
-
-}
